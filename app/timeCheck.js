ceeb1fdeb99b4e4e77f8fefdbfd32927a74e14ea
Jufebrown <jufebrown@gmail.com>
2017-03-29 23:21:32 -0500

    added factories for getting date object, formatting the date and getting sunrise and sunset times

--- app/timeCheck.js
+++ app/timeCheck.js
@@ -1,14 +1,39 @@
 `use strict`
 
-app.factory('timeCheck', function($http){
+app.factory('getDateObjFactory', function($scope) {
+  $scope.dateTime = new Date()
+})
+
+app.factory('formatDateFactory', function($scope) {
   return {
-    let dateTime = new Date()
-    let date = dateTime.getMonth() + '/' + dateTime.getDate() + '/' + dateTime.getFullYear()
-    let tsaUrl = 'http:' + '//' + `apps.tsa.dhs.gov/MyTSAWebService/GetEventInfo.ashx?eventtype=sunrise_sunset&eventdate=${date}&airportcode=BNA&output=json`
-    $http.get(tsaUrl)
-    .then((value) => {
-      console.log(`value`, value)
-      return value.data
-    })
+    formatDate: () => {
+      let year = $scope.dateTime.getFullYear();
+      /// Add 1 because JavaScript months start at 0
+      let month = (1 + $scope.dateTime.getMonth()).toString();
+      month = month.length > 1 ? month : '0' + month;
+      let day = $scope.dateTime.getDate().toString();
+      day = day.length > 1 ? day : '0' + day;
+      let formattedDate = month + '/' + day + '/' + year;
+      return formattedDate
+    }
   }
 })
+
+app.factory('sunriseSunsetFactory', function($http, $scope, formatDateFactory){
+  return {
+    getSunData: () => {
+      let date = formatDate()
+      let tsaUrl = 'http:' + '//' + `apps.tsa.dhs.gov/MyTSAWebService/GetEventInfo.ashx?eventtype=sunrise_sunset&eventdate=${date}&airportcode=BNA&output=json`
+      return $http.get(tsaUrl)
+      .then(function(value) {
+        console.log(`value`, value)
+        return value.data
+      })
+    }
+  }
+})
+
+
+// app.factory('parseTSADataFactory' function(sunriseSunset, $scope) {
+
+// })
