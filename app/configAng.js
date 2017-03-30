361c6d8866a736e12f4b2f0fe4ef4033b01147fb
Jufebrown <jufebrown@gmail.com>
2017-03-30 00:59:28 -0500

    added an otherwise route

--- app/configAng.js
+++ app/configAng.js
@@ -1,14 +1,18 @@
 //assign the angular module to the var app
 var app = angular.module("classList", ['ngRoute']);
+console.log('in configAng.js')
 
 //configure "app" with routeProvider
 app.config(($routeProvider)=> {
   $routeProvider
-    //when at the base page
-    .when("/", {
-      //use the classList Ctrl
-      controller: "classListCtrl",
-      //use the partial "home"
-      templateUrl : "partials/classList.html"
-      })
+  //when at the base page
+  .when("/", {
+    //use the classList Ctrl
+    controller: "classListCtrl",
+    //use the partial "home"
+    templateUrl : "partials/classList.html"
+    })
+  .otherwise({
+    redirectTo: '/'
+  })
 });
